79fc767193f68058e06b9342b3116779d0dc90ea
EvgeniyJet <mas-nira@mail.ru>
2022-03-22 21:09:07 +0700

    add click btn, change number in head

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
-// Вызов модального окна
-$('.button').click(function () {
-	$('.overlay').fadeIn();
-});
+window.onload = function () {
+	let popup = document.querySelector(".popup"),
+		button = document.querySelector(".btn");
+	var card = document.querySelector('.cards .card');
+	button.addEventListener("click", openPop);
 
-// Закрытие окна на крестик
-$('.close-popup').click(function () {
-	$('.overlay').fadeOut();
-});
-
-// Закрытие окна на поле
-$(document).mouseup(function (e) {
-	var popup = $('.popup');
-	if (e.target != popup[0] && popup.has(e.target).length === 0) {
-		$('.overlay').fadeOut();
+	function openPop() {
+		popup.style.display = "block";
 	}
-});
+
+	window.addEventListener("click", closePop);
+
+	function closePop(e) {
+		if (e.target == popup) {
+			popup.style.display = "none";
+		}
+	}
+	card.addEventListener('click', () => {
+		card.classList.toggle('change-bg');
+	});
+	const time = 3000;
+	const step = 2;
+	function outNum(num, elem) {
+		let l = document.querySelector('.' + elem);
+		n = 0;
+		let t = Math.round(time / (num / step));
+		let interval = setInterval(() => {
+			n = n + step;
+			if (n == num) {
+				clearInterval(interval);
+			}
+			l.innerHTML = n;
+		},
+			t);
+
+	}
+	outNum(26, 'age')
+};
