d453aa0efa678156ea31b7e7e64786f6711d98c3
EvgeniyJet <mas-nira@mail.ru>
2022-03-23 18:42:42 +0700

    small change in _media

--- js/script.js
+++ js/script.js
@@ -14,32 +14,32 @@ window.onload = function () {
 			popup.style.display = "none";
 		}
 	}
-
-	const boxIventElems = document.querySelector('.box-ivent');
-	const ageElems = boxIventElems.querySelectorAll('.age');
-	ageElems.forEach(elem => {
-		let num = 26;
-		let time = 2000;
-		let step = 1;
-		let n = 0;
-		let t = Math.round(time / (num / step));
-		let interval = setInterval(() => {
-			n = n + step;
-			if (n == num) {
-				clearInterval(interval);
-			}
-			elem.innerText = n;
-		},t);
-	});	
 };
+const boxIventElems = document.querySelector('.box-ivent');
+const ageElems = boxIventElems.querySelectorAll('.age');
+ageElems.forEach(elem => {
+	let num = 26;
+	let time = 2000;
+	let step = 1;
+	let n = 0;
+	let t = Math.round(time / (num / step));
+	let interval = setInterval(() => {
+		n = n + step;
+		if (n == num) {
+			clearInterval(interval);
+		}
+		elem.innerText = n;
+	}, t);
+});
+
 
 const closureElem = document.querySelector(".closure");
 let yScroll = false;
-window.addEventListener("scroll", function(event){
-    if(!yScroll && this.document.documentElement.getBoundingClientRect().top < -1000) {
-            this.document.querySelector(".band_holder").style.display = "block";
-            yScroll = true; 
+window.addEventListener("scroll", function (event) {
+	if (!yScroll && this.document.documentElement.getBoundingClientRect().top < -1000) {
+		this.document.querySelector(".band_holder").style.display = "block";
+		yScroll = true;
 
-}
-    closureElem.addEventListener('click', ()=> document.querySelector(".band_holder").style.display = "none")
-})
+	}
+	closureElem.addEventListener('click', () => document.querySelector(".band_holder").style.display = "none")
+});
